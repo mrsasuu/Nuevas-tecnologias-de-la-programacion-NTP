d34fa103f366124187788c8450e6bb9d4f3e33ff
mrsasuu <mrsasuu@gmail.com>
2017-05-10 18:31:44 +0200

    Inicio práctica 3

---
+++ Scala/Practica 3/Practica3/src/Conjunto.scala
@@ -0,0 +1,76 @@
+/**
+  * Created by Sasu on 10/05/2017.
+  */
+/**
+  * Clase para representar conjuntos definidos mediante una funcion
+  * caracteristica (un predicado). De esta forma, se declara el tipo
+  * conjunto como un predicado que recibe un entero (elemento) como
+  * argumento y dvuelve un valor booleano que indica si pertenece o no
+  * al conjunto
+  *
+  * @param funcionCaracteristica
+  */
+class Conjunto(val funcionCaracteristica: Int => Boolean) {
+  /**
+    * Crea una cadena con el contenido completo del conjunto
+    *
+    * @return
+    */
+  override def toString(): String = {
+    val elementos = for (i <- -Conjunto.LIMITE to Conjunto.LIMITE
+                         if funcionCaracteristica(i)) yield i
+    elementos.mkString("{", ",", "}")
+  }
+
+  /**
+    * Metodo para determinar la pertenencia de un elemento al
+    * conjunto
+    * @param elemento
+    * @return valor booleano indicando si elemento cumple
+    * la funcion caracteristica o no
+    */
+  def apply(elemento: Int): Boolean = {
+    funcionCaracteristica(elemento)
+  }
+
+  //def union(conjunto1: Conjunto, conjunto2: Conjunto) = ???
+}
+
+
+/**
+  * Objecto companion que ofrece metodos para trabajar con
+  * conjuntos
+  */
+object Conjunto {
+  def union(conjunto1: Conjunto, conjunto2: Conjunto) = ???
+
+  def interseccion(conjunto1: Conjunto, conjunto2: Conjunto) = ???
+
+  def diferencia(conjunto1: Conjunto, conjunto2: Conjunto) = ???
+
+  def filtrar(c : Conjunto, predicado : Int => Boolean) = ???
+
+  def existe(conjunto: Conjunto, function: (Nothing) => Any) = ???
+
+  def paraTodo(conjunto: Conjunto, function: (Nothing) => Any) = ???
+
+
+
+  /**
+    * Limite para la iteracion necesaria algunas operaciones,
+    * entre -1000 y 1000
+    */
+  private final val LIMITE = 1000
+
+  /**
+    * Metodo que permite crear objetos de la clase Conjunto
+    * de forma sencilla
+    * @param f
+    * @return
+    */
+  def apply(f: Int => Boolean): Conjunto = {
+    new Conjunto(f)
+  }
+
+}
+
