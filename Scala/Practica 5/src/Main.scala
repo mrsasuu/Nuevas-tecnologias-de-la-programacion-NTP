9dfd1883ef2dca01b64b26b30363daad5f19833d
mrsasuu <mrsasuu@gmail.com>
2017-06-05 00:04:35 +0200

    Casi acabado
    Faltan comentarios y scalatest

--- Scala/Practica 5/src/Main.scala
+++ Scala/Practica 5/src/Main.scala
@@ -3,12 +3,12 @@
   */
 object Main extends App{
 
-  def calcularPeso(nodo : Nodo) : Int ={
+  def calcularPeso(nodo : Nodo) : Int = nodo match{
     case NodoHoja(_, contador) => contador
     case NodoInterno(nodoIzquierda,nodoDerecha,_,_) => calcularPeso(nodoIzquierda) + calcularPeso(nodoDerecha)
   }
 
-  def obtenerCaracteres(nodo : Nodo ) : List[Char] ={
+  def obtenerCaracteres(nodo : Nodo ) : List[Char] = nodo match{
     case NodoHoja(caracter, _) => List(caracter)
     case NodoInterno(nodoIzquierda,nodoDerecha,_,_) => obtenerCaracteres(nodoIzquierda) ::: obtenerCaracteres(nodoDerecha)
   }
@@ -35,7 +35,7 @@ object Main extends App{
 
     lista = lista.distinct
 
-    lista.sortWith((elemento1, elemento2) => elemento1._2 < elemento2._2)
+    lista = lista.sortWith((elemento1, elemento2) => elemento1._2 < elemento2._2)
 
     lista
 
@@ -53,12 +53,15 @@ object Main extends App{
   }
 
   def combinar(nodos: List[Nodo]): List[Nodo] = {
-    val resultado = (generarArbol(nodos) :: nodos.tail.tail).sortWith((elemento1, elemento2) => calcularPeso(elemento1) < calcularPeso(elemento2))
+    var resultado = (generarArbol(nodos) :: nodos.tail.tail)
+
+    resultado = resultado.sortWith((elemento1, elemento2) => calcularPeso(elemento1) < calcularPeso(elemento2))
 
     resultado
   }
 
   def repetir(singleton: List[Nodo] => Boolean, combinar: List[Nodo] => List[Nodo])(listaNodos : List[Nodo]) :List[Nodo]={
+    println(listaNodos)
     if(singleton(listaNodos)){
       listaNodos
     }else {
@@ -70,10 +73,81 @@ object Main extends App{
   def crearArbolCodificacion(textoCodificar : List[Char]) : Nodo ={
     val listaFrecuencias = frecuenciasAparicion(textoCodificar)
     val listaNodos = crearListaNodosHoja(listaFrecuencias)
+
+
     val resultado = repetir(singleton,combinar)(listaNodos)
 
     resultado.head
   }
+
+
+
+  def decodificar( mensajeSecreto : List[Int],raiz : Nodo ) : List[Char] ={
+    def aux( mensajeSecreto: List[Int],nodo: Nodo): List[Char] ={
+      nodo match {
+        case NodoHoja(letra, _) =>
+          if (mensajeSecreto.isEmpty) {
+            List(letra)
+          }else{
+            letra :: aux(mensajeSecreto,raiz)
+          }
+        case NodoInterno(nodoIzquierda, nodoDerecha, _, _) =>
+          if (mensajeSecreto.head == 0) {
+            aux(mensajeSecreto.tail,nodoIzquierda )              
+          }
+          else {
+            aux(mensajeSecreto.tail, nodoDerecha)
+          }
+      }
+    }
+
+    aux(mensajeSecreto,raiz)
+  }
+
+  type TablaCodigo = List[(Char, List[Int])]
+
+  def convertirArbolTabla(arbolCodificacion: Nodo) : TablaCodigo = {
+
+    def aux(nodo: Nodo, tabla: List[Int]): TablaCodigo = {
+      nodo match {
+        case NodoHoja(letra, _) => List((letra, tabla))
+        case NodoInterno(nodoIzquierda, nodoDerecha, _, _) =>
+          aux(nodoIzquierda, tabla :+ 0) ::: aux(nodoDerecha, tabla :+ 1)
+      }
+    }
+
+    aux(arbolCodificacion, List())
+  }
+
+  def codificarConTabla(tabla : TablaCodigo)(caracter : Char) : List[Int] ={
+    val result = tabla.filter(entrada => entrada._1 == caracter).head._2
+
+    result
+  }
+
+  def rapidoDeDecodificacion(arbolCodificacion: Nodo,textoCodificar: List[Char]): List[Int] = {
+    val tablaCodigo = convertirArbolTabla(arbolCodificacion)
+
+    var resultado : List[Int] = List()
+
+    textoCodificar.foreach(elemento => resultado=  resultado ++ codificarConTabla(tablaCodigo)(elemento))
+
+    resultado
+  }
+
+  def leerFichero(path: String) : List[Char] ={
+    import scala.io.Source
+
+    var resultado : List[Char] = List()
+
+    Source.fromFile(path).getLines().toList.foreach(linea => {
+      println(resultado)
+      resultado = resultado ++ linea.toList})
+
+
+    resultado
+  }
+
 
   val codigoHuffmanFrances: Nodo = NodoInterno(
     NodoInterno(
@@ -152,61 +226,88 @@ object Main extends App{
       List('r', 'c', 'v', 'g', 'b', 'n', 't', 'e', 'i', 'a'), 881025),
     List('s', 'd', 'x', 'j', 'f', 'z', 'k', 'w', 'y', 'h', 'q', 'o', 'l', 'm', 'p', 'u', 'r', 'c', 'v', 'g', 'b', 'n', 't', 'e', 'i', 'a'), 1486387)
 
-   val mensajeSecretoFrances: List[Int] = List(0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1,
-   0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1)
+  val mensajeSecretoFrances: List[Int] = List(0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1,
+    0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1)
 
-  def decodificar( mensajeSecreto : List[Int],raiz : Nodo ) : List[Char] ={
-    def aux( mensajeSecreto: List[Int],nodo: Nodo): List[Char] ={
-      nodo match {
-        case NodoHoja(letra, _) =>
-          if (mensajeSecreto.length == 0) {
-            List(letra)
-          }else{
-            letra :: aux(mensajeSecreto,raiz)
-          }
-        case NodoInterno(nodoIzquierda, nodoDerecha, _, _) =>
-          if (mensajeSecreto.head == 0) {
-            aux(mensajeSecreto.tail,nodoIzquierda )              
-          }
-          else {
-            aux(mensajeSecreto.tail, nodoDerecha)
-          }
-      }
-    }
 
-    aux(mensajeSecreto,raiz)
-  }
+  val mensajeDecodificado: List[Char] = decodificar(mensajeSecretoFrances,codigoHuffmanFrances)
 
-  type TablaCodigo = List[(Char, List[Int])]
+  println("Mensaje decodificado: " + mensajeDecodificado.mkString)
 
-  def convertirArbolTabla(arbolCodificacion: Nodo) : TablaCodigo = {
 
-    def aux(nodo: Nodo, tabla: List[Int]): TablaCodigo = {
-      nodo match {                                                                          // nodo actual
-        case NodoHoja(caracter, _) => List((caracter, tabla))                               // es nodo hoja => devuelve el caracter con la lista hasta llegar a él
-        case NodoInterno(nodoIzquierda, nodoDerecha, _, _) =>                                            // es nodo intermedio => recursividad a izda y dcha agregando un 0 y 1
-          aux(nodoIzquierda, tabla :+ 0) ::: aux(nodoDerecha, tabla :+ 1) //                       a la lista respectivamente
-      }
-    }
+  val codificacionTabla = rapidoDeDecodificacion(codigoHuffmanFrances,mensajeDecodificado)
+println(codificacionTabla)
 
-    aux(arbolCodificacion, List())
-  }
+  println("Mensaje original == mensaje codificado: " + (mensajeSecretoFrances == codificacionTabla))
 
-  def codificarConTabla(tabla : TablaCodigo)(caracter : Char) : List[Int] ={
-    val result = tabla.filter(entrada => entrada._1 == caracter).head._2
+  val arbolOriginal = NodoInterno(
+    NodoHoja('a', 8),
+    NodoInterno(
+      NodoInterno(
+        NodoInterno(
+          NodoHoja('g', 1),
+          NodoHoja('h', 1),
+          List('g','h'), 2),
+        NodoInterno(
+          NodoHoja('e', 1),
+          NodoHoja('f', 1),
+          List('e','f'), 2),
+        List('g','h','e','f'), 4),
+      NodoInterno(
+        NodoInterno(
+          NodoHoja('c', 1),
+          NodoHoja('d', 1),
+          List('c', 'd'), 2),
+        NodoHoja('b', 3),
+        List('c','d','b'), 5),
+      List('g','h','e','f','c','d','b'), 9),
+    List('a','g','h','e','f','c','d','b'), 17)
 
-    result
-  }
+  /**
+    * Cadena con la que construir el árbol
+    */
+  val cadena = List[Char]('a','a','a','a','a','a','a','a','b','b','b','c','d','e','f','g','h')
 
-  def rapidoDeDecodificacion(arbolCodificacion: Nodo,textoCodificar: List[Char]): List[Int] = {
-    val tablaCodigo = convertirArbolTabla(arbolCodificacion)                            // crea la tabla a partir del árbol
+  // Árbol generado
+  val arbol = crearArbolCodificacion(cadena)
 
-    var resultado = List[Int]
+  println(arbol)
+  println("\n__________________\nEspacio\n__________________\n")
+  println(arbolOriginal)
 
-    textoCodificar.foreach(elemento => resultado :: codificarConTabla(tablaCodigo)(elemento))
+  // Debe ser igual a árbol original
+  println("Árbol original == Árbol generado: " + (arbolOriginal == arbol))
 
-    resultado
-  }
+  // Se decodifica este mensaje secreto
+  val mensajeSecretoEsp = List(0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1,
+    0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0,
+    1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0,
+    0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1,
+    1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1)
+
+  val regenta: List[Char] = leerFichero("./src/regenta.txt")
+
+  val arbol2 = crearArbolCodificacion(regenta)
+
+
+
+  println(decodificar(mensajeSecretoEsp,arbol2))
+
+  val mensaje : List[Char] = "La regenta de Benito Perez Galdos".toList
+
+  val codificacionTabla2 = rapidoDeDecodificacion(codigoHuffmanFrances,mensajeDecodificado)
+  println(codificacionTabla2)
+
+  println("mensajecofificadoESP == mensajeSecretoEsp" + codificacionTabla2 == mensajeSecretoEsp)
+
+
+
+
+
+
+
 
 
 
--- Scala/Practica 5/src/NodoInterno.scala
+++ Scala/Practica 5/src/NodoInterno.scala
@@ -2,5 +2,5 @@
   * Created by mrsas on 01/06/2017.
   */
 case class NodoInterno (val hijoIzquierda: Nodo, val hijoDerecha: Nodo, val caracteres: List[Char], val contador: Int) extends Nodo {
-
+  override def toString: String = "Interno: " + caracteres + " -> " + contador + "\n{\n1.- " + hijoIzquierda.toString + "2.- " + hijoDerecha.toString + "}\n"
 }
